d488c51319704e006069406787022e6b61ba4898
Gdecessus <116078803+Gdecessus@users.noreply.github.com>
2026-02-24 14:23:58 +0000

    starting models

---
+++ src/ai_engine/model.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+from tensorflow import keras
+from keras.models import Model
+from keras.layers import Dense, Conv2D, Flatten, BatchNormalization, Activation, Input, Add
+from keras.optimizers import Adam
+
+
+class ChessModel:
+    def __init__(s):
+        
+    
+    def _residual_block():
+      
+    def _build_model(self):
+
+    def predict():
+    
+    def fit():
+  
+    
+    def save():
+        
+    
+    def load():
+       
+    def load_from_file():
+        
