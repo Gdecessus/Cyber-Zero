79ab46c02e693816efc383ac3a62fc874c82ff81
Gdecessus <116078803+Gdecessus@users.noreply.github.com>
2026-02-24 14:24:52 +0000

    few changes

--- src/ai_engine/model.py
+++ src/ai_engine/model.py
@@ -6,8 +6,11 @@ from keras.optimizers import Adam
 
 
 class ChessModel:
-    def __init__(s):
-        
+    def __init__(self, learning_rate=0.002, num_residual_blocks=4, num_filters=64):
+        self.learning_rate = learning_rate
+        self.num_residual_blocks = num_residual_blocks
+        self.num_filters = num_filters
+        self.model = self._build_model()
     
     def _residual_block():
       
