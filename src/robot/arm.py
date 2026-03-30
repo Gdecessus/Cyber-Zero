067500c3ff22306ecbeb95b7ad8c322409e39d20
Gdecessus <116078803+Gdecessus@users.noreply.github.com>
2026-03-30 13:27:43 +0100

    mapping

---
+++ src/robot/arm.py
@@ -0,0 +1,42 @@
+
+{"T":105}
+HAND: 2.78 = open, HAND: 2.99 = closed
+
+
+{"T":102,"base":0.43,"shoulder":0.96,"elbow":0.36,"wrist":1.68,"roll":0.41,"hand":2.78,"spd":0,"acc":10}
+
+H1 pose to DESCEND with open hand
+
+{"T":102,"base":-0.23,"shoulder":0.90,"elbow":0.541495218,"wrist":1.560058461,"roll":-0.263844696,"hand":2.78,"spd":0,"acc":10}
+
+A1 Pose descend with open hands 
+
+
+D4 pose descend with open hands
+
+{"T":102,"base":0.09817477,"shoulder":0.440252486,"elbow":1.63522352,"wrist":1.020097224,"roll":-0.001533981,"hand":2.78,"spd":0,"acc":10}
+
+E4 pose descend with open hands
+
+{"T":102,"base":-0.029145635,"shoulder":0.438718505,"elbow":1.626019635,"wrist":1.010893339,"roll":-0.09664079,"hand":2.78,"spd":0,"acc":10}
+
+
+
+
+
+
+
+HOVER OVER POSITIONS BELOW:
+
+{"T":102,"base":0.381961216,"shoulder":0.601320469,"elbow":0.593650565,"wrist":1.661301193,"roll":0.306796158,"hand":2.99,"spd":0,"acc":10}
+
+This is the pose used for squares f1,f2,g1,g2,h1,h2 hovering over with the gripper closed. HAND: 2.78 = open, HAND: 2.99 = closed
+    
+{"T":102,"base":0.108912636,"shoulder":0.5,"elbow":0.7,"wrist":1.682776924,"roll":-0.01994175,"hand":2.99,"spd":0,"acc":10}
+
+This is the pose used for squares c,d,e 1 and 2 hovering over with the gripper closed. HAND: 2.78 = open, HAND: 2.99 = closed
+    
+{"T":102,"base":-0.15493206,"shoulder":0.527689391,"elbow":0.832951568,"wrist":1.563126423,"roll":-0.262310715,"hand":2.99,"spd":0,"acc":10}
+
+This is the pose used for squares a,b 1,2,3 hovering over with the gripper closed. HAND: 2.78 = open, HAND: 2.99 = closed
+    
